9c6243b6dcf9e020274681d36d4d5ad27cce1fb5
Sarang Nitin Jagdale <sarangjagda2020@gmail.com>
2026-07-13 23:52:43 +0530

    Creation of Class and Object in Java

---
+++ CreationofClassandObjects.java
@@ -0,0 +1,21 @@
+
+
+    class student{
+        String Name;
+        int Age;
+
+    }
+    public class CreationofClassandObjects{
+
+    public static void main(String[] args){
+
+        student S1 = new student() ;
+
+        S1.Name = "Sarang" ;
+        S1.Age = 20 ;
+
+        System.out.println("Name is " +S1.Name) ;
+        System.out.println("Age is " + S1.Age) ;
+    }
+    }
+
