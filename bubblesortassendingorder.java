a2c5892ac7f2139d63f2b34f4d9f9a6b03fed848
Sarang Nitin Jagdale <sarangjagda2020@gmail.com>
2026-07-20 22:15:37 +0530

    Bubble Sort in Assending Order

---
+++ bubblesortassendingorder.java
@@ -0,0 +1,22 @@
+public class bubblesortassendingorder {
+
+    public static void main(String[]  args) {
+        int[] arr = {5, 2, 9, 1, 5, 6};
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i ; j++)
+            {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+
+                }
+            }
+        }
+        for (int num : arr) {
+            System.out.print(num + "");
+
+        }
+    }
+}
